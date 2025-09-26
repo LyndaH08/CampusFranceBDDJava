18a99b2c53c33964ad8b6290189524371b33f6b9
Hachemi Lynda <lyndahachemi95@gmail.com>
2025-09-27 00:13:10 +0200

    update runner

--- src/test/java/org/example/runner/RunCucumberTest.java
+++ src/test/java/org/example/runner/RunCucumberTest.java
@@ -7,13 +7,14 @@ import org.junit.platform.suite.api.Suite;
 import io.cucumber.junit.platform.engine.Cucumber;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 
 //@Suite
-@Cucumber
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features") // le dossier des .feature
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.example.steps") // package des s
+@SelectClasspathResource("features") // dossier des .feature
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.example.steps") // package des step definitions
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-reports/Cucumber.html, json:target/cucumber-reports/Cucumber.json")
 public class RunCucumberTest {
 }
